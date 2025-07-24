d64073c7419e6352b905d02341eba8cbc6f17c4d
Cameron Chetty <ST10251759@vcconnect.edu.za>
2025-07-24 14:29:59 +0200

    Adding SSL (HTTPS)

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const mongoose = require('mongoose'); // this will be used later
+const mongoose = require('mongoose');
 const app = require('./app');
+const https = require('https');
+const fs = require('fs');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const options = {
+  key: fs.readFileSync('ssl/key.pem'),
+  cert: fs.readFileSync('ssl/cert.pem'),
+};
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log('Server running at https://localhost:' + PORT);
 });
